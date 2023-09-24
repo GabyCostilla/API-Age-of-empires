ec45e8e48f567e41b3b5df2f0ebcc994d8011170
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2023-09-24 10:54:32 -0300

    arreglamos los problemas de la lista desplegable

--- Age_of_empires_api/src/civilizacion.js
+++ Age_of_empires_api/src/civilizacion.js
@@ -4,34 +4,45 @@ import axios from 'axios';
 import Search from './search';
 
 function App() {
+  const [originalCivilizations, setOriginalCivilizations] = useState([]); // Mantén la lista original aquí
   const [civilizations, setCivilizations] = useState([]);
   const [filterType, setFilterType] = useState('');
 
   useEffect(() => {
     axios.get('/api/civilizations')
       .then(response => {
-        if (Array.isArray(response.data)) {
-          setCivilizations(response.data);
-        } else {
-          console.error('La respuesta no es un arreglo:', response.data);
-        }
+        setOriginalCivilizations(response.data); // Guarda la lista original
+        setCivilizations(response.data);
       })
       .catch((error) => console.error('Error:', error));
   }, []);
 
   const handleFilterChange = (event) => {
     setFilterType(event.target.value);
+    filterCivilizations(originalCivilizations, event.target.value); // Filtra la copia con el nuevo valor
   };
 
   const handleSearch = (filteredCivilizations) => {
     setCivilizations(filteredCivilizations);
   };
 
+  // Función para filtrar la lista de civilizaciones
+  const filterCivilizations = (list, type) => {
+    if (type === '') {
+      setCivilizations(list); // Si no hay filtro, muestra la lista completa
+    } else {
+      const filteredCivilizations = list.filter((civilization) => {
+        return civilization.army_type === type;
+      });
+      setCivilizations(filteredCivilizations); // Muestra solo las civilizaciones filtradas
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <Search
-          civilizations={civilizations}
+          civilizations={originalCivilizations} // Pasa la lista original
           filterType={filterType}
           onSearch={handleSearch}
         />
