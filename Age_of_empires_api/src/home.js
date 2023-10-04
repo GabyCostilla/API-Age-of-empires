6a0955fb9da4a85b456074679da1456ebe7d3343
GabyCostilla <d47722877@alumnos.itr.edu.ar>
2023-10-04 10:51:01 -0300

    zdfad

--- Age_of_empires_api/src/home.js
+++ Age_of_empires_api/src/home.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Home.css"
+
 function Home() {
   return (
     <div className="App-header">
@@ -12,6 +13,12 @@ function Home() {
       <p>
         Explora las civilizaciones, descubre sus fortalezas y debilidades, y
         prepárate para liderar a tu pueblo hacia la grandeza.
+      </p>
+      <p>
+        Este proyecto fue desarrollado por Gaby Costilla y Marcos Ariza en colaboración con el Instituto Técnico Renault.
+      </p>
+      <p>
+        ¡Síguenos en Instagram: <a href="https://www.instagram.com/gabnzana_en_compota/">@gabnzana_en_compota</a> y contáctanos en Gmail: <a href="mailto:gabrielckeret@gmail.com">gabrielckeret@gmail.com</a>!
       </p>
     </div>
   );
